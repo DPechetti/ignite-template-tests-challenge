cd508efa70b5b8f9ec045219b5e8ccf84007878e
Donovan Pechetti <dovi_pf@hotmail.com>
2022-01-07 15:05:59 -0300

    transfer operation route created

--- src/routes/statements.routes.ts
+++ src/routes/statements.routes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import { CreateStatementController } from '../modules/statements/useCases/createStatement/CreateStatementController';
 import { GetBalanceController } from '../modules/statements/useCases/getBalance/GetBalanceController';
 import { GetStatementOperationController } from '../modules/statements/useCases/getStatementOperation/GetStatementOperationController';
+import { TransferOperationController } from '../modules/statements/useCases/transferOperation/TransferOperationController';
 import { ensureAuthenticated } from '../shared/infra/http/middlwares/ensureAuthenticated';
 
 const statementRouter = Router();
 const getBalanceController = new GetBalanceController();
 const createStatementController = new CreateStatementController();
+const transferOperationController = new TransferOperationController();
 const getStatementOperationController = new GetStatementOperationController();
 
 statementRouter.use(ensureAuthenticated);
@@ -16,5 +18,6 @@ statementRouter.get('/balance', getBalanceController.execute);
 statementRouter.post('/deposit', createStatementController.execute);
 statementRouter.post('/withdraw', createStatementController.execute);
 statementRouter.get('/:statement_id', getStatementOperationController.execute);
+statementRouter.post('/transfer/:user_id', transferOperationController.execute)
 
 export { statementRouter };
